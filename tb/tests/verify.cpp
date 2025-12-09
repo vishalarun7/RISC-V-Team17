44d4b0d9c7d05152a31ba5d660e4261701802556
Raphael Sinai <raphsinai@icloud.com>
2025-12-09 21:57:01 +0000

    added basic f1 functionality with display out cycle's output

--- tb/tests/verify.cpp
+++ tb/tests/verify.cpp
@@ -1,5 +1,8 @@
 #include <cstdlib>
 #include <utility>
+#include <string>
+#include <iostream>
+#include "vbuddy.cpp"
 
 #include "cpu_testbench.h"
 
@@ -44,6 +47,38 @@ TEST_F(CpuTestbench, TestPdf)
     initSimulation();
     runSimulation(CYCLES * 100);
     EXPECT_EQ(top_->a0, 15363);
+}
+
+TEST_F(CpuTestbench, F1)
+{
+    setupTest("f1");
+    initSimulation();
+    if (vbdOpen() != 1)
+        FAIL();
+    vbdHeader("F1 RISC-V");
+    vbdSetMode(1);
+    vbdBar(0);
+
+    int cyc;
+    int tick;
+    
+    for (cyc = 0; cyc < CYCLES; cyc++) {
+        runSimulation(1);
+
+        vbdCycle(cyc);
+        vbdBar(top_->a0 & 0xFF);
+        vbdHex(3,(int(top_->a0)>>8)&0xF);
+        vbdHex(2,(int(top_->a0)>>4)&0xF);
+        vbdHex(1,int(top_->a0)&0xF);
+
+        if (Verilated::gotFinish()) {
+            vbdClose();
+            exit(0);
+        }
+    }
+
+    vbdClose();
+    exit(0);
 }
 
 int main(int argc, char **argv)
