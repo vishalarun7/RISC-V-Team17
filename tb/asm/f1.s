d784aa28883cdf266b81ea93afe6436e0c00b799
Raphael Sinai <raphsinai@icloud.com>
2025-12-12 14:02:23 +0000

    f1.s

---
+++ tb/asm/f1.s
@@ -0,0 +1,21 @@
+.text
+.globl main
+
+main:
+    LI a0, 0
+    LI a1, 0
+    LI s1, 8
+
+_loop:
+    BGE a1, s1, finish
+    JAL t1, incr
+    J _loop
+
+incr:
+    ADDI a1, a1, 1
+    SLLI a2, a0, 1
+    ADDI a0, a2, 1
+    JALR x0, 0(t1)  
+
+finish:
+    J finish
